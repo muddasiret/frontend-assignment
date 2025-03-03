57685d9e12f0e98948913476231db0a72928586f
muddasiret <muddasir009@gmail.com>
2025-03-03 18:07:45 +0530

    feat: pagination button added and pagination function implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+const PAGE_LIMIT = 5;
 
 function App() {
-	const [tableData, setTableData] = useState(null);
+	const [apiData, setApiData] = useState(null);
+	const [paginatedData, setPaginatedData] = useState(null);
+	const [page, setPage] = useState(0);
 
 	useEffect(() => {
 		async function getData() {
@@ -14,7 +17,8 @@ function App() {
 					throw new Error(`Response status: ${response.status}`);
 				}
 				const parsedResponse = await response.json();
-				setTableData(parsedResponse);
+				setApiData(parsedResponse);
+				setPaginatedData(parsedResponse.slice(0, PAGE_LIMIT));
 			} catch (error) {
 				console.error(error.message);
 			}
@@ -22,26 +26,65 @@ function App() {
 		getData();
 	}, []);
 
+	useEffect(() => {
+		if (apiData) {
+			setPaginatedData(
+				apiData.slice(PAGE_LIMIT * page, (page + 1) * PAGE_LIMIT)
+			);
+		}
+	}, [page]);
+
+	const handlePageChange = (type = "next") => {
+		if (type === "next") {
+			setPage((page) => page + 1);
+		} else {
+			setPage((page) => page - 1);
+		}
+	};
+
+	const isPreviousBtnDisabled = page === 0;
+	const isNextBtnDisabled = page > apiData.length / PAGE_LIMIT - 1;
+
 	return (
 		<>
 			<h3>Frontend assignment</h3>
-			{tableData ? (
-				<table>
-					<tr>
-						<th>S.No.</th>
-						<th>Percentage funded</th>
-						<th>Amount pledged</th>
-					</tr>
-					{tableData.map((item) => {
-						return (
-							<tr>
-								<td>{item["s.no"]}</td>
-								<td>{item["percentage.funded"]}</td>
-								<td>{item["amt.pledged"]}</td>
-							</tr>
-						);
-					})}
-				</table>
+			{paginatedData ? (
+				<>
+					<table>
+						<tr>
+							<th>S.No.</th>
+							<th>Percentage funded</th>
+							<th>Amount pledged</th>
+						</tr>
+						{paginatedData.map((item) => {
+							return (
+								<tr>
+									<td>{item["s.no"]}</td>
+									<td>{item["percentage.funded"]}</td>
+									<td>{item["amt.pledged"]}</td>
+								</tr>
+							);
+						})}
+					</table>
+					<div className="pagination">
+						<button
+							disabled={isPreviousBtnDisabled}
+							onClick={() => {
+								handlePageChange("previous");
+							}}
+						>
+							Previous
+						</button>
+						<button
+							onClick={() => {
+								handlePageChange("next");
+							}}
+							disabled={isNextBtnDisabled}
+						>
+							Next
+						</button>
+					</div>
+				</>
 			) : (
 				"Loading fund data"
 			)}
